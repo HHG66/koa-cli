6122cdcb5a72924de908a910507b8d52377de397
韩宏广 <2421578700@qq.com>
2023-02-09 08:16:33 +0800

    基础结构

---
+++ src/controller/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: HHG
+ * @Date: 2023-02-08 22:40:35
+ * @LastEditTime: 2023-02-08 22:40:36
+ * @LastEditors: 韩宏广
+ * @FilePath: /koa-cli/src/controller/index.js
+ * @文件说明: 
+ */
---
+++ src/service/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: HHG
+ * @Date: 2023-02-08 22:40:45
+ * @LastEditTime: 2023-02-08 22:40:45
+ * @LastEditors: 韩宏广
+ * @FilePath: /koa-cli/src/service/index.js
+ * @文件说明: 
+ */
