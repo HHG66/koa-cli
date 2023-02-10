7280f2b672f19d854ca076d7915e32ca5c6227c6
韩宏广 <2421578700@qq.com>
2023-02-10 23:08:49 +0800

    基本机构完成

--- src/controller/test/index.js
+++ src/controller/test/index.js
@@ -1,4 +1,6 @@
 module.exports = (ctx, next) => {
-  console.log(ctx.request.body);
+  // console.log(ctx.request.body);
+  console.log('处理路由参数');
+  require('../../service/test/index.js')(ctx, next)
 }
 
