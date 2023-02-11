d5ed55f8819568b83d6252e3155d1bd18cef4d59
韩宏广 <2421578700@qq.com>
2023-02-11 11:41:20 +0800

    修改webpack配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,19 @@
 /*
  * @Author: HHG
  * @Date: 2023-02-09 10:34:44
- * @LastEditTime: 2023-02-11 01:22:05
+ * @LastEditTime: 2023-02-11 11:22:37
  * @LastEditors: 韩宏广
  * @FilePath: /koa-cli/webpack.config.js
  * @文件说明: 
  */
 const path = require('path')
-// console.log(process.cwd() + '/src');
-// console.log(path.resolve(__dirname, './src/'));
-console.log(path.resolve("src"));
-const Koa = require('koa')
- 
-const webpack = require('webpack');
 module.exports = {
   // target: 'node18.12',
   target:'node',
-  // node: {
-  //   __dirname: true,
-  // },
-  // devtool: 'inline-source-map',
+  devtool: 'inline-source-map',
   mode: "development", // "production" | "development" | "none"
-  // context: path.resolve(__dirname, 'dist'),
   entry: {
     main: path.resolve(__dirname, './src/app.js')
-    // main: './main.js'
   },
   devServer: {
     static: './dist/main.js',
