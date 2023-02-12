391a607b7438ff95174bcec1901259222a3741d6
韩宏广 <2421578700@qq.com>
2023-02-12 18:36:18 +0800

    连接mongo数据库。初始化数据

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: HHG
  * @Date: 2023-02-10 12:07:23
- * @LastEditTime: 2023-02-12 02:09:50
+ * @LastEditTime: 2023-02-12 18:13:08
  * @LastEditors: 韩宏广
  * @FilePath: /koa-cli/src/router/index.js
  * @文件说明: 
@@ -41,69 +41,6 @@ class InitController {
         InitController.app.use(obj.allowedMethods())
       }
     }
-  }
-
-  static deepClone(target) {
-    const map = new WeakMap()
-
-    function isObject(target) {
-      return (typeof target === 'object' && target) || typeof target === 'function'
-    }
-
-    function clone(data) {
-      if (!isObject(data)) {
-        return data
-      }
-      if ([Date, RegExp].includes(data.constructor)) {
-        return new data.constructor(data)
-      }
-      if (typeof data === 'function') {
-        return new Function('return ' + data.toString())()
-      }
-      const exist = map.get(data)
-      if (exist) {
-        return exist
-      }
-      if (data instanceof Map) {
-        const result = new Map()
-        map.set(data, result)
-        data.forEach((val, key) => {
-          if (isObject(val)) {
-            result.set(key, clone(val))
-          } else {
-            result.set(key, val)
-          }
-        })
-        return result
-      }
-      if (data instanceof Set) {
-        const result = new Set()
-        map.set(data, result)
-        data.forEach(val => {
-          if (isObject(val)) {
-            result.add(clone(val))
-          } else {
-            result.add(val)
-          }
-        })
-        return result
-      }
-      const keys = Reflect.ownKeys(data)
-      const allDesc = Object.getOwnPropertyDescriptors(data)
-      const result = Object.create(Object.getPrototypeOf(data), allDesc)
-      map.set(data, result)
-      keys.forEach(key => {
-        const val = data[key]
-        if (isObject(val)) {
-          result[key] = clone(val)
-        } else {
-          result[key] = val
-        }
-      })
-      return result
-    }
-
-    return clone(target)
   }
 
 }
--- src/router/testrouter/index.js
+++ src/router/testrouter/index.js
@@ -1,7 +1,12 @@
-// const Router = require('@koa/router');
-// let router = new Router();
+const Router = require('@koa/router');
+// const router = new Router({
+//   prefix: '/admin'
+// });
 
 let routerName = '/test'
+router.opts={
+  prefix: '/admin'
+}
 router.post(routerName, (ctx, next) => {
   console.log('进入路由');
   require(`controller/${routerName}/index.js`)(ctx, next)
